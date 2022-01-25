559129e99fe52cf2c082c28592347d9e41e7cacf
Bosko Stupar <bosko.stupar@quantox.com>
2022-01-25 13:32:26 +0100

    init

--- src/PaymentAdapterClass.php
+++ src/PaymentAdapterClass.php
@@ -1,14 +1,6 @@
 <?php
 
-/*
- * Copyright 2017 SCTR Services
- *
- * Distribution and reproduction are prohibited.
- *
- * @package     www.bang.com
- * @copyright   SCTR Services LLC 2017
- * @license     No License (Proprietary)
- */
+
 namespace Quantox;
 
 use Quantox\Interfaces\CreditCardInterface;
